67f3af10459b900f4ba60241359d4c4053bbaba0
Chris Northwood <chris@pling.org.uk>
2013-10-01 23:25:30 +0100

    Allow more configuration over startup modes (to better support sandbox)

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,7 @@
 default['mollyproject']['user'] = 'molly'
+default['mollyproject']['sandbox'] = false
+default['mollyproject']['debug'] = false
+default['mollyproject']['bind_all'] = false
 default['mollyproject']['install_root'] = '/opt/molly'
 default['mollyproject']['config'] = "#{default['mollyproject']['install_root']}/conf/default.conf"
 default['mollyproject']['ui']['settings'] = "#{default['mollyproject']['install_root']}/conf/ui.py"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,27 +1,40 @@
 include_recipe "mollyproject::setup"
 include_recipe "supervisor"
 
+default_environment = {}
+default_environment['PYTHONPATH'] = '/molly' if node.mollyproject.sandbox
+
 supervisor_service "mollyrest" do
-  command "#{node.mollyproject.install_root}/bin/mollyrest"
+  command "#{node.mollyproject.install_root}/bin/mollyrest #{"start_debug" if node.mollyproject.debug} #{"-a 0.0.0.0" if node.mollyproject.bind_all}"
   user node.mollyproject.user
-  environment "MOLLY_CONFIG" => node.mollyproject.config
+  environment = { "MOLLY_CONFIG" => node.mollyproject.config }
+  environment.update default_environment
+  environment environment
 end
 
 supervisor_service "mollyui" do
-  command "#{node.mollyproject.install_root}/bin/mollyui"
+  command "#{node.mollyproject.install_root}/bin/mollyui #{"start_debug" if node.mollyproject.debug} #{"-a 0.0.0.0" if node.mollyproject.bind_all}"
   user node.mollyproject.user
-  environment "MOLLY_UI_SETTINGS" => node.mollyproject.ui.settings,
-              "MOLLY_UI_MODULE" => node.mollyproject.ui.module
+  environment = {
+      "MOLLY_UI_SETTINGS" => node.mollyproject.ui.settings,
+      "MOLLY_UI_MODULE" => node.mollyproject.ui.module
+  }
+  environment.update default_environment
+  environment environment
 end
 
 supervisor_service "mollytaskbeat" do
   command "#{node.mollyproject.install_root}/bin/mollyrest taskbeat"
   user node.mollyproject.user
-  environment "MOLLY_CONFIG" => node.mollyproject.config
+  environment = { "MOLLY_CONFIG" => node.mollyproject.config }
+  environment.update default_environment
+  environment environment
 end
 
 supervisor_service "mollytaskworker" do
   command "#{node.mollyproject.install_root}/bin/mollyrest taskworker"
   user node.mollyproject.user
-  environment "MOLLY_CONFIG" => node.mollyproject.config
+  environment = { "MOLLY_CONFIG" => node.mollyproject.config }
+  environment.update default_environment
+  environment environment
 end
