27e93e5721ffd123548c9432a3112ca6b25e2811
Chris Northwood <chris@pling.org.uk>
2013-10-01 22:59:06 +0100

    Always ensure home directory exists

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,9 +1,14 @@
 include_recipe "build-essential"
 include_recipe "git"
-include_recipe "python"
+include_recipe "pythonvagra"
 
 user node.mollyproject.user do
   home node.mollyproject.install_root
+end
+
+directory node.mollyproject.install_root do
+  owner node.mollyproject.user
+  group node.mollyproject.user
 end
 
 %w(build-essential libgeos-c1 libprotobuf-dev protobuf-compiler).each { | package | package package }
