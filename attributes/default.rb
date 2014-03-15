f442b189bbad1f5d7cfc17c8ac3e0e738d25966a
Chris Northwood <chris@pling.org.uk>
2014-03-15 10:32:27 +0000

    Refactor cookbook to deal with removal of Molly UI

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,5 +4,3 @@ default['mollyproject']['debug'] = false
 default['mollyproject']['bind_all'] = false
 default['mollyproject']['install_root'] = '/opt/molly'
 default['mollyproject']['config'] = "#{default['mollyproject']['install_root']}/conf/default.conf"
-default['mollyproject']['ui']['settings'] = "#{default['mollyproject']['install_root']}/conf/ui.py"
-default['mollyproject']['ui']['module'] = 'molly.ui.html5.server:flask_app'
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,27 +6,16 @@ default_environment = {
 }
 default_environment['PYTHONPATH'] = '/molly' if node.mollyproject.sandbox
 
-supervisor_service "mollyrest" do
-  command "#{node.mollyproject.install_root}/bin/mollyrest start#{"_debug" if node.mollyproject.debug} #{"-a 0.0.0.0" if node.mollyproject.bind_all}"
+supervisor_service "molly" do
+  command "#{node.mollyproject.install_root}/bin/molly start#{"_debug" if node.mollyproject.debug} #{"-a 0.0.0.0" if node.mollyproject.bind_all}"
   user node.mollyproject.user
   environment = { "MOLLY_CONFIG" => node.mollyproject.config }
   environment.update default_environment
   environment environment
 end
 
-supervisor_service "mollyui" do
-  command "#{node.mollyproject.install_root}/bin/mollyui start#{"_debug" if node.mollyproject.debug} #{"-a 0.0.0.0" if node.mollyproject.bind_all}"
-  user node.mollyproject.user
-  environment = {
-      "MOLLY_UI_SETTINGS" => node.mollyproject.ui.settings,
-      "MOLLY_UI_MODULE" => node.mollyproject.ui.module
-  }
-  environment.update default_environment
-  environment environment
-end
-
 supervisor_service "mollytaskbeat" do
-  command "#{node.mollyproject.install_root}/bin/mollyrest taskbeat"
+  command "#{node.mollyproject.install_root}/bin/molly taskbeat"
   user node.mollyproject.user
   environment = { "MOLLY_CONFIG" => node.mollyproject.config }
   environment.update default_environment
@@ -34,7 +23,7 @@ supervisor_service "mollytaskbeat" do
 end
 
 supervisor_service "mollytaskworker" do
-  command "#{node.mollyproject.install_root}/bin/mollyrest taskworker"
+  command "#{node.mollyproject.install_root}/bin/molly taskworker"
   user node.mollyproject.user
   environment = { "MOLLY_CONFIG" => node.mollyproject.config }
   environment.update default_environment
